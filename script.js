2e9fea5c4747ca89c7565e7b1663e93b0b599816
dmilani-br <douglass.bianchi@hotmail.com>
2026-01-18 19:41:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// Estrutura pronta para futuros slides
-console.log("Political Opinion loaded");
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+setInterval(() => {
+  slides[current].classList.remove("active");
+  current = (current + 1) % slides.length;
+  slides[current].classList.add("active");
+}, 6000);
